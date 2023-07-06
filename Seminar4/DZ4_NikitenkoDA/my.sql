9b1ee84a686c3113dc3531c433303e3dd5179f5d
DenNik <nikidenal@mail.ru>
2023-07-06 21:19:22 +0500

    Seminar4 and DZ4

--- Seminar4/DZ4_NikitenkoDA/my.sql
+++ Seminar4/DZ4_NikitenkoDA/my.sql
@@ -5,12 +5,12 @@ CREATE TABLE groupmate (
   address TEXT NOT NULL
 );
 
-INSERT INTO groupmate(name, age, address) VALUES ('Anton', '18', 'Moscow');
-INSERT INTO groupmate(name, age, address) VALUES ('Olga', '19', 'Samara');
-INSERT INTO groupmate(name, age, address) VALUES ('Pauline', '18', 'Kirov');
-INSERT INTO groupmate(name, age, address) VALUES ('Fedor', '30', 'Moscow');
-INSERT INTO groupmate(name, age, address) VALUES ('Mark', '17', 'Ekaterinburg');
-INSERT INTO groupmate(name, age, address) VALUES ('Klark', '22', 'Moscow');
-INSERT INTO groupmate(name, age, address) VALUES ('Maria', '35', 'Moscow');
+INSERT INTO groupmate(name, age, address) VALUES ('Anton', 18, 'Moscow');
+INSERT INTO groupmate(name, age, address) VALUES ('Olga', 19, 'Samara');
+INSERT INTO groupmate(name, age, address) VALUES ('Pauline', 18, 'Kirov');
+INSERT INTO groupmate(name, age, address) VALUES ('Fedor', 30, 'Moscow');
+INSERT INTO groupmate(name, age, address) VALUES ('Mark', 17, 'Ekaterinburg');
+INSERT INTO groupmate(name, age, address) VALUES ('Klark', 22, 'Moscow');
+INSERT INTO groupmate(name, age, address) VALUES ('Maria', 35, 'Moscow');
 
 SELECT name FROM groupmate WHERE address = 'Moscow' AND age >= 18 AND age < 30;
